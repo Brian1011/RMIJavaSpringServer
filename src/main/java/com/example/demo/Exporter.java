8baea0ca7d63ee39c274b57f700e3eb4e580e93a
Brian1011 <mutindabrian26@gmail.com>
2019-10-28 11:30:19 +0300

    fix server error

--- src/main/java/com/example/demo/Exporter.java
+++ src/main/java/com/example/demo/Exporter.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import com.example.demo.service.CabBookingService;
 import com.example.demo.service.CabBookingServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.rmi.RmiServiceExporter;
 import org.springframework.remoting.support.RemoteExporter;
 
 // this makes the service available to the clients
+@Configuration
 public class Exporter {
+
+    @Bean
     RemoteExporter RegisterRMIExporter(){
         RmiServiceExporter exporter = new RmiServiceExporter();
         exporter.setServiceName("CabBookingService");
